64d690c7a196ca82c65c300f8763f856a4d14565
Snugglez <32309975+Snugglez@users.noreply.github.com>
2018-02-25 15:15:44 -0500

    Added Toggle Command
    For those that magically die in ghillie to not lose their run

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ This script will reset all dungeons
 
 Useful for those that go from ghillie right to Velik's Sanctuary
 Removes the need to /reset while in Velik's Sanctuary
+
+Use command `!ggr` or `ggr` if in proxy chat to disable/enable
+
+Requires Command to function
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const config = require('./config.json');
+const Command = require('command');
 module.exports = function ggreset(dispatch) {
-dispatch.hook('S_LOAD_TOPO', 1, (event) => {           
+const command = Command(dispatch)
+let enabled = config.toggle;
+command.add('ggr', () => {
+enabled = !enabled
+command.message(`GG-Reset is now ${enabled ? 'enabled' : 'disabled'}.`)})
+dispatch.hook('S_LOAD_TOPO', 1, (event) => {
+if(!enabled) return;
 if(event.zone === 9714)
 dispatch.toServer('C_RESET_ALL_DUNGEON', {
 })})}
