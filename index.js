d8d1f3f9549f7ca43ac453dcd9ac154318ff7b47
Snugglez <32309975+Snugglez@users.noreply.github.com>
2018-04-12 09:32:36 -0400

    vecMEME.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let enabled = config.toggle;
 command.add('ggr', () => {
 enabled = !enabled
 command.message(`GG-Reset is now ${enabled ? 'enabled' : 'disabled'}.`)})
-dispatch.hook('S_LOAD_TOPO', 1, (event) => {
+dispatch.hook('S_LOAD_TOPO', 3, (event) => {
 if(!enabled) return;
 if(event.zone === 9714)
 dispatch.toServer('C_RESET_ALL_DUNGEON', {
